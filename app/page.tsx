feff04cdad70a23af50a1f33763aa633bbc46ff4
André Albano <andre_barros@id.uff.br>
2026-01-30 23:28:04 -0300

    Added hero ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Navbar from "./ui/navigation/navbar";
+import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
 
 export default function Home() {
   return (
     <>
       <p>Your code, everywhere</p>
+      <Card>
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
     </>
   );
 }
